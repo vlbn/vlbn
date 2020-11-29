f5dfde6ab1c9fce29560701c1d55e796bef735d7
vlbn <cf.valbuena@gmail.com>
2020-11-30 00:00:03 +0100

    mouse pointer added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="centerXY fill-viewport-100 pointer" @click="flipCard">
-    <transition name="fade" mode="out-in">
-      <div v-if="!flip">
-        <Logo />
-      </div>
-      <div v-else class="centerXY">
-        <Name class="mb-6" />
-        <ContactData class="mt-6" />
-      </div>
-    </transition>
+  <div class="centerXY fill-viewport-100" @mousemove="mousePointerAttach">
+
+    <div ref="mousePointerA" class="mouse-pointer-a"></div>
+    <div ref="mousePointerB" class="mouse-pointer-b"></div>
+
+    <div @mousedown="flipCard" class="pointer spot">
+      <transition name="fade" mode="out-in">
+        <div v-if="!flip">
+          <Logo />
+        </div>
+        <div v-else class="centerXY">
+          <Name class="mb-6" />
+          <ContactData class="mt-6 pointer" />
+        </div>
+      </transition>
+    </div>
+
   </div>
 </template>
 
@@ -17,7 +24,10 @@ import Logo from "@/components/card/Logo.vue";
 import Name from "@/components/card/Name.vue";
 import ContactData from "@/components/card/ContactData.vue";
 
+import { mousePointer } from "@/mixins/mousePointer";
+
 export default {
+  mixins: [mousePointer],
   name: "Home",
   components: {
     Logo,
