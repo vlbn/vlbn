b8bb2ee32f842f6fc99acafefacb5c651ff42092
vlbn <cf.valbuena@gmail.com>
2020-12-04 01:51:14 +0100

    added landScape mediaquery

--- src/mixins/handleMediaQueries.js
+++ src/mixins/handleMediaQueries.js
@@ -5,22 +5,27 @@ export const handleMediaQueries = {
     const mediaQuery1 = window.matchMedia("(max-width: 1024px)");
     const mediaQuery2 = window.matchMedia("(min-width: 1025px)");
     const mediaQuery3 = window.matchMedia("(orientation: portrait)");
+    const mediaQuery4 = window.matchMedia("(orientation: landscape)");
 
     mediaQuery1.addListener(this.handleDeviceMobile);
     mediaQuery2.addListener(this.handleDeviceDesktop);
     mediaQuery3.addListener(this.handleDevicePortrait);
+    mediaQuery4.addListener(this.handleDeviceLandscape);
 
     this.handleDeviceMobile(mediaQuery1);
     this.handleDeviceDesktop(mediaQuery2);
     this.handleDevicePortrait(mediaQuery3);
+    this.handleDeviceLandscape(mediaQuery4);
 
     console.log("handling media queries");
+
   },
   computed: {
     ...mapState("mediaQueries", [
       "deviceMobile",
       "deviceDesktop",
       "devicePortrait",
+      "deviceLandscape",
     ]),
   },
   methods: {
@@ -28,6 +33,7 @@ export const handleMediaQueries = {
       "handleDeviceMobile",
       "handleDeviceDesktop",
       "handleDevicePortrait",
+      "handleDeviceLandscape",
     ]),
   },
 };
--- src/store/modules/mediaQueries.js
+++ src/store/modules/mediaQueries.js
@@ -2,6 +2,7 @@ const state = () => ({
   deviceMobile: null,
   deviceDesktop: null,
   devicePortrait: null,
+  deviceLandscape: null,
 });
 
 const mutations = {
@@ -14,6 +15,9 @@ const mutations = {
   handleDevicePortrait(state, e) {
     state.devicePortrait = e.matches;
   },
+  handleDeviceLandscape(state, e) {
+    state.deviceLandscape = e.matches;
+  },  
 };
 
 export default {
