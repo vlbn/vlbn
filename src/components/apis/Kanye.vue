1941e181eb3585ae2660d7488ac85eacb2bfdd3d
vlbn <cf.valbuena@gmail.com>
2022-03-23 18:37:28 +0100

    format

--- src/components/apis/Kanye.vue
+++ src/components/apis/Kanye.vue
@@ -1,22 +1,19 @@
 <script setup>
-
 import { ref } from "vue";
-
 import KanyeAPI from "@/services/KanyeAPI";
 
-const quote = ref('');
+const quote = ref("");
 
 const loadQuote = async () => {
-    try {
-        const response = await KanyeAPI.getQuote()
-        quote.value = response.data.quote
-    } catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    const response = await KanyeAPI.getQuote();
+    quote.value = response.data.quote;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-loadQuote()
-
+loadQuote();
 </script>
 
 <template>{{ quote }}</template>
