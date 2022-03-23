c5cab9d9f8ebafc4a430606ce9d6ca6e5eb66927
vlbn <cf.valbuena@gmail.com>
2022-03-23 18:46:12 +0100

    routing done right

--- src/views/Hmmm.vue
+++ src/views/Hmmm.vue
@@ -1,22 +1,14 @@
 <script setup>
-
-import router from "@/router";
-
-import {
-    ExclamationIcon,
-} from "@heroicons/vue/outline";
-
-const goTo = (value) => {
-    router.push(value)
-}
-
+import { useRouter } from "vue-router";
+import { ExclamationIcon } from "@heroicons/vue/outline";
+const router = useRouter();
 </script>
 
 <template>
-    <div @click="goTo('/')" class="fill-viewport-100 centerXY pointer">
-        <div class="flex m-1 error">
-            <ExclamationIcon class="icono" />
-            <mark class="m-1">{{ $route.params.catchAll }}</mark> ?
-        </div>
+  <div @click="router.push('/')" class="fill-viewport-100 centerXY pointer">
+    <div class="flex m-1 error">
+      <ExclamationIcon class="icono" />
+      <mark class="m-1">{{ $route.params.catchAll }}</mark> ?
     </div>
+  </div>
 </template>
