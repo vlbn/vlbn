04b6eba1df83a843edcaf4d375654d2dfbd669e4
vlbn <valbuena@duck.com>
2024-02-01 15:40:37 +0100

    view in progress

--- src/views/SweeT.vue
+++ src/views/SweeT.vue
@@ -1,18 +1,9 @@
+<script setup>
+import { inject, ref } from "vue";
+const stores = inject("stores");
+const lloret = ref(stores.state.lloret);
+</script>
+
 <template>
-    <div>
-        ¡Bienvenido a Sweet Lloret Suites! Información relevante para tu estancia:
-
-➡️🚪Código de acceso: XXXX# (edificio y habitación X - piso X)
-
-👉🏻Información importante:
-· La hora de salida es hasta las 11:00; si deseas una salida tardía o prolongar tu estancia, consulta la disponibilidad.
-· No está permitido fumar en las habitaciones.
-· El servicio de limpieza durante la estancia es gratuito; deja las toallas en el suelo si deseas que las cambiemos y retira tus pertenencias de la cama.
-· Si quieres mejorar tu estancia con extras, visita la página "EXPERIENCIAS" en el enlace de check-in proporcionado anteriormente.
-· WIFI: SWEET LLORET GUEST - Contraseña: Sweet2023@
-
-Si necesitas algo, no dudes en hacérnoslo saber.
-
-¡Que tengas una agradable estancia! 🙂
-    </div>
+  <div>{{ lloret }}</div>
 </template>
