e3f4ab4a521476f35abac744e33008b3157deab4
vlbn <valbuena@duck.com>
2024-02-01 14:41:58 +0100

    hello simplicity

--- src/views/NoEncontrado.vue
+++ src/views/NoEncontrado.vue
@@ -1,9 +1,6 @@
-<script setup>
-import router from "@/router";
-</script>
-
 <template>
-  <div class="is-flex is-justify-content-center fv-100 is-flex-direction-column is-align-items-center">
-    <span @click="router.push('/')"><mark>404</mark></span>
+  <div @click="$router.push('/')"
+    class="fv-100 centerXY iPoint">
+    <h1>404</h1>
   </div>
 </template>
