09ca8c83f5ceb9f2e36d8afcad6f5857651f6739
vlbn <valbuena@duck.com>
2024-02-04 22:13:01 +0100

    again.. why this?

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import router from "./router";
-import stores from "./stores";
 
 import "./assets/scss/_index.scss";
 
 const app = createApp(App);
 
 app.use(router);
-
-app.provide("stores", stores);
-
 app.mount("#app");
